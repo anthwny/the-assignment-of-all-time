80bddba7db26587ef960d915f6ad753f36e540c8
anthony <anthony75368@gmail.com>
2024-09-12 13:25:18 -0400

    made vro0 fr

---
+++ text/text.py
@@ -0,0 +1,11 @@
+def main():
+    brainrot = 0
+    print("test")
+    bruh = open(f"vro{brainrot}.txt", "w")
+    bruh.write("lol")
+    bruh.close()
+    brainrot += 1
+    pass
+
+if __name__ == "__main__":
+    main()
